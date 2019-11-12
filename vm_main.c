e313ff8706081494a729376b62adcdc4d9493314
RonanM168 <116338731@umail.ucc.ie>
2019-11-12 16:03:55 +0000

    Declared variables

--- vm_main.c
+++ vm_main.c
@@ -1,3 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>      /* for exit() */
+#include<ctype.h>     /* for isdigit(char ) */
+#include<string.h>
+
+char *integer = "LOADINT";
+char *flt = "LOADFLT";
+char *multiply = "MUL\n";
+char *divide = "DIV\n";
+char *add = "ADD\n";
+char *subtract = "SUB\n";
+
+FILE *fptr;
+
+struct Stack* stack;
+
 /* main function begins */
 int main(int argc, char *argv[])
 {
