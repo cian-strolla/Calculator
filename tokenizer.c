b7a1cb23aa438c65d4b4428575870c50e3e5c2d1
cian-strolla <55984948+cian-strolla@users.noreply.github.com>
2019-11-12 15:37:43 +0000

    Rearranged Tokenizer.
    Rearranged Tokenizer to contain separate functions as opposed to everything being in main().

--- tokenizer.c
+++ tokenizer.c
@@ -3,75 +3,70 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int N = 999; //variable for total length of input
+#define N 999
 
-int main() {
+char str[N];
+char str2[N*2];
+char period = '.';
+const char s[2] = " ";
+char *token;
 
-  char period = '.';
-
+int loadStringFromFile(char *inputfile) {
   FILE *input;
-  char str[N];  //will be full unprocessed string from input file
-  char* inputfile = "tokenizer_input.txt";
-  input = fopen(inputfile, "r");  //open input file in read mode
-  while (fgets(str, N, input) != NULL)  //get string from file and store in 'str'
+  input = fopen(inputfile, "r");
+  while (fgets(str, N, input) != NULL)
   fclose(input);
+  return(0);
+}
 
-  char str2[N*2]; //will contain original string with each character separated by a space
-
+int toToken() {
   for (int i = 0; i < N; i++) { //loops through original string, adding a space between each char
     str2[2*i] = str[i];
     if (i != N - 1) {
       str2[2*i + 1] = ' ';
     }
   }
-
-
   for (int u = 0; u < sizeof(str2); u++) {  // function to ensure double digit numbers remain on same line
     if (isdigit(str2[u])) {
-
       if (isdigit(str2[u + 2])) {
-
         for (int j = u + 1; j < sizeof(str2); j++) {
           str2[j] = str2[j+1];
         }
-
       }
       if (str2[u + 2] == period) {
-
         for (int j = u + 1; j < sizeof(str2); j++) {
           str2[j] = str2[j+1];
         }
-
         for (int j = u + 1; j < sizeof(str2); j++) {
           str2[j + 1] = str2[j + 2];
         }
-
       }
     }
   }
-
-
-
-  const char s[2] = " ";  //delim setting for token splitting
-
-  char *token;
-
   token = strtok(str2, s);  //strtok function splitting string into tokens
+  return(0);
+}
 
+
+int printToFile(char *outputfile) {
   FILE *output;
-  char* outputfile = "tokenizer_output.txt";
   output = fopen(outputfile, "w");  //open output file in write mode
-
   while (token != NULL) { //loop through tokens and print to output file
-
-    // printf("%s\n", token); use to print to screen
-
     fprintf(output, "%s\n", token);
-
     token = strtok(NULL, s);
   }
   fclose(output); //close output file
+  return(0);
+}
 
+
+int main() {
+
+  loadStringFromFile("input.txt");
+
+  toToken();
+
+  printToFile("infix.txt");
 
   return(0);
 }
