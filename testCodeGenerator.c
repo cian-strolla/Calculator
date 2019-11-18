ada54cbd1b6f9e49c707434a61d36eea12ae398e
awcflavin <55809901+awcflavin@users.noreply.github.com>
2019-11-18 18:54:05 +0000

    Updated names of files from remote

--- testCodeGenerator.c
+++
@@ -1,8 +0,0 @@
-#include <ctap.h>
-
-int codeGenerator (char *input_string);
-char *testInput = "3 * 2 + 3.24 - 1 / 4";
-char *testOutput = "LOADINT 3\nMUL\nLOADINT 2\nADD\nLOADFLT 3.24\nSUB\nLOADINT 1\nDIV\nLOADINT 4\n";
-TESTS {
-  is(codeGenerator(testInput), testOutput, "Input postfix should correspond to output code");
-}
