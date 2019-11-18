a384917e81b4388fb0fc468b1d28f3befad99682
RonanM168 <116338731@umail.ucc.ie>
2019-11-18 12:26:04 +0000

    Remove the print postfix expression.

--- i2p_main.c
+++ i2p_main.c
@@ -14,8 +14,6 @@ int main(int argc, char *argv[])
 	readfile("infix.txt");
 
 	InfixToPostfix();                   /* call to convert */
-	printf("Postfix Expression: ");
-	puts(postfix);                     /* print postfix expression */
 
 	writefile("postfix.txt");
 
